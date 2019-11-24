e72a17f60f8206811bacbadb1a5c2bc74d9f4fad
bdxyyxh <1345077430@qq.com>
2019-11-24 16:31:11 +0800

    下拉

--- js/teaindex.js
+++ js/teaindex.js
@@ -6,16 +6,38 @@ window.onload = function(){
 			laydate = layui.laydate, laytpl = layui.laytpl,
 			element = layui.element;
 	
-		/*加载表格*/
-		table.render({
-			elem : '#project',
-			id:'project',
-			url : 'http://localhost:8080/SchoolPsychologicalAssessmentWeb/teaindex/geteplanlist',
-			title : '后台通知管理数据表',
-			height: "full-160",
-			skin : 'line',
-			even : true,
-			cols : [ 
+		
+			querylist();
+
+			/* 点击查询对网站用户进行筛选 */
+			$("#btnselfrontinfo").click(function() {
+				//alert("1");
+				querylist(); //调用局部刷新
+
+			});
+
+			function querylist() {
+				var url = conf.apiurl + "/teaindex/geteplanlist";
+				var type = +$("#queryrole").val();
+				var strwhere = document.getElementById("title").value; 
+				var value = strwhere.value;
+				if (type != 10 && !strwhere==undefined) {
+					url = conf.apiurl + "/teaindex/geteplanliststrwhere?type=" + type + "&strwhere = " + value;
+				}else if(type != 10 && strwhere==undefined){
+					url = conf.apiurl + "/teaindex/geteplanliststrwhere?type=" + type+ "&strwhere = ''";
+				}else if(!strwhere==undefined){
+					url = conf.apiurl + "/teaindex/geteplanliststrwhere?type=10 & strwhere = " + value;
+				}
+				alert(url)
+				table.render({
+					elem: '#table',
+					id: 'table',
+					url: url,
+					title: '后台管理员用户数据表',
+					height: "full-160",
+					skin: 'line',
+					even: true,
+					cols: [ 
 			     [ {field : 'eplanid', title:'编号', align:'center'}
 			      ,{field : 'examname', title:'试卷名称', align:'center'}
 				  ,{field : 'starttime',title : '测评时间',align : 'center'}
@@ -24,10 +46,18 @@ window.onload = function(){
                   ,{title : '操作',toolbar : '#barDemo',align : 'center',}
 			     ] 
 			 ],
-			 page: true,
-		});
+					page: {
+						layout: ['prev', 'page', 'next', 'skip', 'count', 'limit'],
+						groups: 5,
+						limit: 10,
+						limits: [10, 20, 30, 40, 50],
+						theme: '#1E9FFF',
+					},
+				});
+			}
+			/*点击查询加载表格数据结束*/
 		//监听工具条
-		table.on('tool(project)',function(obj){
+		table.on('tool(table)',function(obj){
 			var data = obj.data;
 			if(obj.event === 'edit'){
 			layer.msg('ID：'+ data.id + ' 的查看操作');
@@ -40,6 +70,7 @@ window.onload = function(){
 			layer.alert('编辑行：<br>'+ JSON.stringify(data))
 			}
 		});
+
 	});
 
 
